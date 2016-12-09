ae26cc3f09e4775b304f42c280d018dcdab6790d
Maciek <mdrabicki@outlook.com>
2016-12-09 14:09:25 +0100

    before delete itemsContext 2nd try

---
+++ src/MyNotesWall/Data/WallsDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNotesWall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyNotesWall.Data
+{
+    public class WallsDbContext : DbContext
+    {
+        public virtual DbSet<Wall> Walls { get; set; }
+        public  WallsDbContext(DbContextOptions<WallsDbContext> options)
+            : base(options)
+        {
+
+        }
+        public void OnOnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Wall>()
+                .HasMany(s => s.Items)
+                .WithOne(s => s.Wall)
+                .HasForeignKey(s => s.Wall);
+            builder.Entity<Wall>()
+                .HasMany(s => s.UsersWithAccess);
+                
+        }
+    }
+}
